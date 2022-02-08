5fd5aad3bff516932a25d97781d66f6c5d356838
danghai <danghai.ema@gmail.com>
2022-02-08 12:13:23 +0100

    Add get request in server.js and repair some function for rendering in script.js

--- server.js
+++ server.js
@@ -7,3 +7,23 @@ const { getRandomElement } = require("./utils");
 const PORT = process.env.PORT || 4001;
 
 app.use(express.static("public"));
+app.listen(PORT, () => {
+  console.log(`Listening port ${PORT}`);
+});
+app.get("/api/quotes/random", (req, res) => {
+  const random_quote = getRandomElement(quotes);
+  res.send({quote: random_quote});
+});
+app.get("/api/quotes", (req, res) => {
+  if (!req.query.person) {
+    res.send({ quotes: quotes});
+  } else {
+    const person = req.query.person;
+    const quote_of_person = quotes.find((item) => item.person === person);
+    if (quote_of_person) {
+      res.send({quotes: quote_of_person});
+    } else {
+      res.status(400).send();
+    }
+  }
+});
