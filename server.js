8d5da52f4cbea986b54042776a2063145147d2d3
danghai <danghai.ema@gmail.com>
2022-02-08 14:47:23 +0100

    Replace file data.js by data.json, add function post, put, delete in server.js and auto write data in data.json

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require("express");
+const fs = require("fs");
 const app = express();
 
-const { quotes } = require("./data");
+let quotes = [];
+fs.readFile("./data.json", "utf-8", (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    quotes = JSON.parse(data);
+  }
+});
 const { getRandomElement } = require("./utils");
 
 const PORT = process.env.PORT || 4001;
@@ -12,18 +20,72 @@ app.listen(PORT, () => {
 });
 app.get("/api/quotes/random", (req, res) => {
   const random_quote = getRandomElement(quotes);
-  res.send({quote: random_quote});
+  res.send({ quote: random_quote });
 });
 app.get("/api/quotes", (req, res) => {
   if (!req.query.person) {
-    res.send({ quotes: quotes});
+    res.send({ quotes: quotes });
   } else {
     const person = req.query.person;
     const quote_of_person = quotes.find((item) => item.person === person);
     if (quote_of_person) {
-      res.send({quotes: quote_of_person});
+      res.send({ quotes: quote_of_person });
     } else {
       res.status(400).send();
     }
+  }
+});
+app.post("/api/quotes", (req, res) => {
+  const quote = req.query.quote;
+  const person = req.query.person;
+  if (quote && person) {
+    quotes.push({ quote: quote, person: person });
+    fs.writeFile("./data.json", JSON.stringify(quotes), (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+    res.send({ quote: quote, person: person });
+  } else {
+    res.status(400).send();
+  }
+});
+app.put("/api/quotes", (req, res) => {
+  const person = req.query.person;
+  const new_quote = req.query.quote;
+  if (person && new_quote) {
+    const found = quotes.find((item) => item.person === person);
+    if (found) {
+      found.quote = new_quote;
+      fs.writeFile("./data.json", JSON.stringify(quotes), (err) => {
+        if (err) {
+          console.log(err);
+        }
+      });
+      res.send({ person: person, quote: new_quote });
+    } else {
+      res.status(404).send();
+    }
+  } else {
+    res.status(404).send();
+  }
+});
+app.delete("/api/quotes", (req, res) => {
+  const person = req.query.person;
+  if (person) {
+    const found = quotes.find((item) => item.person === person);
+    if (found) {
+      quotes = quotes.filter((item) => item.person !== person);
+      fs.writeFile("./data.json", JSON.stringify(quotes), (err) => {
+        if (err) {
+          console.log(err);
+        }
+      });
+      res.status(204).send();
+    } else {
+      res.status(404).send('Person not found')
+    }
+  } else {
+    res.status(404).send('Parameter empty');
   }
 });
